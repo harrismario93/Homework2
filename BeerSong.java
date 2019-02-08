17ec859bd45c7c64449c5e908ebba085fd230c75
Harris <harrismario93@yahoo.com>
2019-02-09 00:18:55 +0530

    Implemented Ninety_Nine_Bottles_of_Beer function

--- BeerSong.java
+++ BeerSong.java
@@ -2,7 +2,16 @@ public class BeerSong
 { 
     public static void Ninety_Nine_Bottles_of_Beer()
     {
-
+    	int i=99;
+    	for (i=3;i>1;i--) {
+    		System.out.format("%d bottles of beer on the wall, %d bottles of beer\n",i,i);
+    		if (i-1 != 1)
+    			System.out.format("take one down, pass it around, %d bottles of beer on the wall.\n", i-1);
+    		else
+    			System.out.format("take one down, pass it around, %d bottle of beer on the wall.\n", i-1);
+    	}
+    	System.out.format("%d bottle of beer on the wall, %d bottle of beer\n",i,i);
+    	System.out.format("take one down, pass it around, no more bottles of beer on the wall.\n", i-1);
     }
     public static void main(String args[]) 
     { 
